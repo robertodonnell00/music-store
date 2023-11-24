39d5cb7b7fafb70ebcc001ce505a175b571d3f13
robertodonnell00 <robertodonnell76@yahoo.ie>
2023-11-24 17:26:45 +0000

    Reworked Code to get rid of InstrumentAPI and extra functionalities

--- src/main/kotlin/models/Customer.kt
+++ src/main/kotlin/models/Customer.kt
@@ -1,13 +1,85 @@
 package models
 
+
+
 class Customer(
-    var customerID: String,
+    var customerID: Int,
     var customerName: String,
     var customerAddress: String,
-    var itemsBought: String,
+    var itemsBought: MutableSet<Instrument> = mutableSetOf(),
     var vipCustomer: Boolean,
-    var preferredInstrument: String)  {
+    var preferredInstrument: String) {
     override fun toString(): String {
-        return "Note(customerID='$customerID', customerName=$customerName, customerAddress='$customerAddress', itemsBought=$itemsBought, vipCustomer= $vipCustomer, preferredInstrument= $preferredInstrument)"
+        return "Customer(customerID='$customerID', customerName=$customerName, customerAddress='$customerAddress', itemsBought=$itemsBought, vipCustomer= $vipCustomer, preferredInstrument= $preferredInstrument)"
     }
+
+    private fun formatListString(instrumentToFormat: MutableSet<Instrument>): String =
+        instrumentToFormat
+            .joinToString("\n") { instrument ->
+                itemsBought.indexOf(instrument).toString() + ": " + instrument.toString()
+            }
+
+
+    //CREATE
+
+    private var lastInstrument = 0
+    private fun getID() = lastInstrument++
+
+    fun create(instrument: Instrument): Boolean {
+        instrument.instrumentID = getID()
+        return itemsBought.add(instrument)
+    }
+
+    //READ
+
+    fun isValidListIndex(index: Int, list: MutableSet<Instrument>): Boolean {
+        return (index >= 0 && index < list.size)
+    }
+
+    fun isValidIndex(index: Int): Boolean {
+        return isValidListIndex(index, itemsBought)
+    }
+
+    fun listInstruments(): String =
+        if (itemsBought.isEmpty()) "No instruments are stored"
+        else formatListString(itemsBought)
+
+    fun findInstrument(id: Int): Instrument? {
+        return itemsBought.find { instrument -> instrument.instrumentID == id }
+    }
+
+
+    //UPDATE
+    fun updateInstrument(indexToUpdate: Int, instrument: Instrument): Boolean {
+        //find instrument using index
+        val foundInstrument = findInstrument(indexToUpdate)
+        //if instrument exists, use details as parameters to update instrument
+        if (foundInstrument != null) {
+            foundInstrument.instrumentID = instrument.instrumentID
+            foundInstrument.instrumentType = instrument.instrumentType
+            foundInstrument.instrumentReview = instrument.instrumentReview
+            foundInstrument.price = instrument.price
+            foundInstrument.qauntityBought = instrument.qauntityBought
+            foundInstrument.isPaidFor = instrument.isPaidFor
+            foundInstrument.instrumentReview = instrument.instrumentReview
+            foundInstrument.dateReceived = instrument.dateReceived
+            foundInstrument.customerBoughtID = instrument.customerBoughtID
+            return true
+        }
+        return false
+    }
+
+
+    //DELETE
+
+    fun delete(id: Int): Boolean {
+        return itemsBought.removeIf { instrument -> instrument.instrumentID == id }
+    }
+
+
+    //PERSISTENCE
+
 }
+
+
+
--- src/main/kotlin/models/Instrument.kt
+++ src/main/kotlin/models/Instrument.kt
@@ -3,16 +3,16 @@ package models
 import java.util.Date
 
 class Instrument(
-    var instrumentID: String,
+    var instrumentID: Int,
     var instrumentType: String,
     var price: Double,
     var qauntityBought: Int,
-    var isPaidFor: Boolean,
-    var instrumentReview: Int,// x out of 10
+    var isPaidFor: Boolean = true,
+    var instrumentReview: Int, // x out of 10
     var dateReceived: String,
-    var customerBought: String
+    var customerBoughtID: Customer
 ) {
     override fun toString(): String {
-        return "Note(instrumentID='$instrumentID', instrumentType=$instrumentType, price='$price', qauntityBought=$qauntityBought, isPaidFor= $isPaidFor, instrumentReview= $instrumentReview, dateReceived= $dateReceived, customerBought= $customerBought)"
+        return "Note(instrumentID='$instrumentID', instrumentType=$instrumentType, price='$price', qauntityBought=$qauntityBought, isPaidFor= $isPaidFor, instrumentReview= $instrumentReview, dateReceived= $dateReceived, customerBought= $customerBoughtID)"
     }
 }
